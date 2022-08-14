1c84875cdc55493c93bf6afae68fef09006b1e4b
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2022-08-14 08:16:10 +0700

    Initial commit

---
+++ SK-Lib-cstdio-ungetc-Bagian7__CPP/Source.cpp
@@ -0,0 +1,33 @@
+#pragma warning(disable:4996)
+
+#include <cstdio>
+#include <conio.h>
+
+/*
+    Source by AlphaCodingSkills (https://www.alphacodingskills.com)
+    Modified For Learn by RK
+    I.D.E : VS2022
+*/
+
+int main() {
+    int ch;
+    char buffer[256];
+
+    FILE* pFile = fopen("test.txt", "r");
+
+    while ((ch = getc(pFile)) != EOF) {
+        if (ch == '.') {
+            ungetc('!', pFile);
+        } else {
+            ungetc(ch, pFile);
+        }
+
+        ch = getc(pFile);
+        putchar(ch);
+    }
+
+    fclose(pFile);
+
+    _getch();
+    return 0;
+}
